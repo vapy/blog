9a0c71dd5665a7309c01afa836c1772ad8994bc3
vapy <870915876@qq.com>
2016-09-28 15:53:27 +0800

    main4

---
+++ blogCode/src/com/vapy/offer/Main4.java
@@ -0,0 +1,47 @@
+package com.vapy.offer;
+/**
+ *
+ * @author vapy 2016年9月28日
+ *
+ */
+import java.util.Scanner;
+
+public class Main4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 0;
+		if (sc.hasNextInt()) {
+			count = sc.nextInt();
+			sc.nextLine();
+		}
+		String[] input = new String[count];
+		for (int i = 0; i < count; i++) {
+			input[i] = sc.nextLine();
+		}
+		func(input);
+		for (String s : input) {
+			System.out.println(s);
+		}
+	}
+
+	public static void func(String[] input) {
+		for(int i = 0, len = input.length; i < len; i ++) {
+			input[i] = func2(input[i]);
+		}
+	}
+
+	public static String func2(String str) {
+		String[] temp = str.split(" ");
+		String result = "";
+		for (int i = temp.length - 1; i >= 0; i--) {
+			if (i == 0) {
+				result += temp[i];
+			}
+			else {
+				result = result + temp[i] + " ";
+			}
+		}
+		return result;
+	}
+}
+
